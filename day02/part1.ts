eb2395b7293b1abf87a06ec2ade372114c1c7d8b
Jeffrey Harrington <jeffharrington@airbook.local>
2023-12-02 15:24:48 -0500

    Cleanup

--- day02/part1.ts
+++ day02/part1.ts
@@ -9,24 +9,21 @@ import { dirname } from "path";
 const process = (lines: string[]) => {
     const regex = /(\d+) (blue|red|green)(;?)/gm;
     const possibleGameIndexes = lines.map((line, index) => {
-        const cubes: Record<string, number> = {
-            red: 0,
-            green: 0,
-            blue: 0,
-        };
-        const matches = [...line.matchAll(regex)];
-        const possibilities = matches.map((match, match_index) => {
-            cubes[match[2]] += parseInt(match[1]);
-            if (match[3] === ";" || match_index === matches.length - 1) {
-                if (!(cubes.red <= 12 && cubes.green <= 13 && cubes.blue <= 14)) {
-                    return false; // not possible
-                }
-                cubes.red = 0;
-                cubes.green = 0;
-                cubes.blue = 0;
-            }
-            return true; // possible
-        });
+        const possibilities = line
+            .split(": ")[1]
+            .split("; ")
+            .map((cubesLine) => {
+                const cubes: Record<string, number> = {
+                    red: 0,
+                    green: 0,
+                    blue: 0,
+                };
+                const matches = [...cubesLine.matchAll(regex)];
+                matches.forEach((match) => {
+                    cubes[match[2]] += parseInt(match[1]);
+                });
+                return cubes.red <= 12 && cubes.green <= 13 && cubes.blue <= 14;
+            });
         return possibilities.every((possible) => possible) ? index + 1 : 0;
     });
     const possibleGameIndexesSum = possibleGameIndexes.reduce((a, b) => a + b, 0);
