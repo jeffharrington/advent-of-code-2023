82580cc3443aefc13c809a6e05cb0e5ffe016c7b
Jeffrey Harrington <jeffharrington@airbook.local>
2023-12-02 15:31:59 -0500

    Cleanup

--- day02/part2.ts
+++ day02/part2.ts
@@ -9,28 +9,27 @@ import { dirname } from "path";
 const process = (lines: string[]) => {
     const regex = /(\d+) (blue|red|green)(;?)/gm;
     const powers = lines.map((line) => {
-        const cubes: Record<string, number> = {
-            red: 0,
-            green: 0,
-            blue: 0,
-        };
         const minimums: Record<string, number> = {
             red: 0,
             green: 0,
             blue: 0,
         };
-        const matches = [...line.matchAll(regex)];
-        matches.forEach((match, match_index) => {
-            cubes[match[2]] += parseInt(match[1]);
-            if (match[3] === ";" || match_index === matches.length - 1) {
+        line.split(": ")[1]
+            .split("; ")
+            .map((cubesLine) => {
+                const cubes: Record<string, number> = {
+                    red: 0,
+                    green: 0,
+                    blue: 0,
+                };
+                const matches = [...cubesLine.matchAll(regex)];
+                matches.forEach((match) => {
+                    cubes[match[2]] += parseInt(match[1]);
+                });
                 minimums.red = Math.max(minimums.red, cubes.red);
                 minimums.green = Math.max(minimums.green, cubes.green);
                 minimums.blue = Math.max(minimums.blue, cubes.blue);
-                cubes.red = 0;
-                cubes.green = 0;
-                cubes.blue = 0;
-            }
-        });
+            });
         return minimums.red * minimums.green * minimums.blue;
     });
     const powersSum = powers.reduce((a, b) => a + b, 0);
