19f3275c6520e8102f2772c3069920d9a168b281
Jeff Harrington <jeff@cedar.com>
2023-12-04 11:27:04 -0500

    Day 4

--- day04/part1.ts
+++ day04/part1.ts
@@ -9,15 +9,15 @@ import { dirname } from "path";
 const process = (lines: string[]) => {
     const scores = lines.map((line) => {
         const [_, allNumbersStr] = line.split(": ");
-        const [winning_str, card_str] = allNumbersStr.split(" | ");
-        const winning_numbers = new Set(
-            winning_str.split(" ").flatMap((str) => (str.trim() ? [parseInt(str.trim())] : [])),
+        const [winningStr, cardStr] = allNumbersStr.split(" | ");
+        const winningNumbers = new Set(
+            winningStr.split(" ").flatMap((str) => (str.trim() ? [parseInt(str.trim())] : [])),
         );
-        const card_numbers = new Set(
-            card_str.split(" ").flatMap((str) => (str.trim() ? [parseInt(str.trim())] : [])),
+        const cardNumbers = new Set(
+            cardStr.split(" ").flatMap((str) => (str.trim() ? [parseInt(str.trim())] : [])),
         );
         const commonNumbers = new Set(
-            Array.from(card_numbers).filter((num) => winning_numbers.has(num)),
+            Array.from(cardNumbers).filter((num) => winningNumbers.has(num)),
         );
         const score = commonNumbers.size > 0 ? 2 ** (commonNumbers.size - 1) : 0;
         return score;
