6f4444c86659184cf0b21e580ab39336d053572b
Jeff Harrington <jeff@cedar.com>
2023-12-08 10:57:00 -0500

    Cleanup

--- day08/part2.ts
+++ day08/part2.ts
@@ -10,50 +10,36 @@ const process = (lines: string[]) => {
     const moves = lines.shift() || "";
     lines.shift(); // Skip blank line
     const regex = /^(\w+) = \((\w+), (\w+)\)$/;
-    const table: Record<string, string[]> = {};
     const startingNodes: string[] = [];
-    lines.forEach((line) => {
-        // console.log(line);
+    const table = lines.reduce((acc: Record<string, string[]>, line) => {
         const match = regex.exec(line);
         if (match) {
             const [, node, left, right] = match;
-            // console.log(node, left, right);
-            table[node] = [left, right];
+            acc[node] = [left, right];
             if (node[2] === "A") {
                 startingNodes.push(node);
             }
         }
-    });
-
-    console.log(startingNodes);
-    // console.log(table);
-    let i = 0;
-    let num_steps = 0;
+        return acc;
+    }, {});
+    let step = 0;
     let currNodes = Array.from(startingNodes);
-    console.log("Starting nodes:", currNodes);
     const stepsTable: Record<string, number> = {};
     while (true) {
-        // if (currNodes.every((node) => node[2] === "Z")) {
-        //     break;
-        // }
-        const move = moves[i];
+        const move = moves[step % moves.length];
         currNodes = currNodes.map((node) => {
             if (node[2] === "Z") {
-                stepsTable[node] = num_steps;
+                stepsTable[node] = step;
             }
             const [left, right] = table[node];
             const next = move === "L" ? left : right;
-            console.log("move", node, move, "to", next);
             return next;
         });
         if (Object.keys(stepsTable).length === startingNodes.length) {
             break;
         }
-        i += 1;
-        i %= moves.length;
-        num_steps += 1;
+        step += 1;
     }
-    console.log(stepsTable);
     const totalSteps = findLCM(Object.values(stepsTable));
     return totalSteps;
 };
