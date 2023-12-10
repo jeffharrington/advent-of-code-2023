9d107b3fd8e4847fcde0f4662a6a8d5a5a6c7083
Jeff Harrington <jeff@cedar.com>
2023-12-10 15:11:07 -0500

    Cleanup

--- day10/part1.ts
+++ day10/part1.ts
@@ -7,53 +7,46 @@ import { dirname } from "path";
  * https://adventofcode.com/2023/day/10
  */
 const process = (lines: string[]) => {
-    let matrix: string[][] = [];
-    const distance: Record<string, number> = {};
-    let startingPoint: number[] = [0, 0];
-    lines.forEach((line, row) => {
-        matrix.push(line.split(""));
-        if (line.includes("S")) {
-            startingPoint = [row, line.indexOf("S")];
-        }
-    });
-    let curr = startingPoint;
-    const validCoords = getValidCoords(startingPoint, matrix);
-    const leftDistance = visit(startingPoint, validCoords[0], matrix, { [curr.toString()]: 0 }, 1);
-    const rightDistance = visit(startingPoint, validCoords[1], matrix, { [curr.toString()]: 0 }, 1);
-    const finalDistance: Record<string, number> = {};
-    Object.keys(leftDistance).forEach((key) => {
-        finalDistance[key] = Math.min(leftDistance[key], rightDistance[key]);
-    });
-    return Math.max(...Object.values(finalDistance));
+    const matrix: string[][] = lines.map((line) => line.split(""));
+    const startingPoint = matrix.reduce(
+        (acc, curr, row_index) => {
+            const colIndex = curr.indexOf("S");
+            if (colIndex !== -1) {
+                return [row_index, colIndex];
+            } else {
+                return acc;
+            }
+        },
+        [0, 0],
+    );
+    const validCoords = getNodes(startingPoint, matrix);
+    const leftDistance = visit(validCoords[0], matrix, { [startingPoint.toString()]: 0 }, 1);
+    const rightDistance = visit(validCoords[1], matrix, { [startingPoint.toString()]: 0 }, 1);
+    const bestDistances = Object.keys(leftDistance).map((key) =>
+        Math.min(leftDistance[key], rightDistance[key]),
+    );
+    return Math.max(...bestDistances);
 };
 
-function visit(
-    start: number[],
-    coord: number[],
-    matrix: string[][],
-    distance: Record<string, number>,
-    step: number,
-) {
-    distance[coord.toString()] = step;
-    const validCoords = getValidCoords(coord, matrix);
+function visit(coord: number[], matrix: string[][], visited: Record<string, number>, step: number) {
+    visited[coord.toString()] = step;
+    const nodes = getNodes(coord, matrix);
     if (
-        Object.keys(distance).includes(validCoords[0].toString()) &&
-        Object.keys(distance).includes(validCoords[1].toString())
+        Object.keys(visited).includes(nodes[0].toString()) &&
+        Object.keys(visited).includes(nodes[1].toString())
     ) {
-        return distance;
+        return visited;
     }
-    const next = Object.keys(distance).includes(validCoords[1].toString())
-        ? validCoords[0]
-        : validCoords[1];
-    return visit(start, next, matrix, distance, step + 1);
+    const next = Object.keys(visited).includes(nodes[1].toString()) ? nodes[0] : nodes[1];
+    return visit(next, matrix, visited, step + 1);
 }
 
-function getValidCoords(coord: number[], matrix: string[][]): number[][] {
+function getNodes(coord: number[], matrix: string[][]): number[][] {
     const NORTH = [-1, 0];
     const SOUTH = [1, 0];
     const EAST = [0, 1];
     const WEST = [0, -1];
-    const directionMap: Record<string, number[][]> = {
+    const allowedDirectionsMap: Record<string, number[][]> = {
         "|": [SOUTH, NORTH],
         "-": [EAST, WEST],
         "L": [NORTH, EAST],
@@ -66,44 +59,35 @@ function getValidCoords(coord: number[], matrix: string[][]): number[][] {
     const validSouthBound = ["|", "J", "L", "S"];
     const validEastBound = ["-", "J", "7", "S"];
     const validWestBound = ["-", "F", "L", "S"];
-    const north = getNorth(coord);
-    const east = getEast(coord);
-    const south = getSouth(coord);
-    const west = getWest(coord);
-    let validCoords = [];
     const shape = matrix[coord[0]][coord[1]];
-    const validDirections = directionMap[shape];
-    if (validDirections.includes(NORTH) && validNorthBound.includes(matrix[north[0]][north[1]])) {
-        validCoords.push(north);
+    const allowedDirections = allowedDirectionsMap[shape];
+    let validCoords = [];
+    if(allowedDirections.includes(NORTH)) {
+        const northCoord = [coord[0] - 1, coord[1]];
+        if(validNorthBound.includes(matrix[northCoord[0]][northCoord[1]])) {
+            validCoords.push(northCoord);
+        }
     }
-    if (validDirections.includes(EAST) && validEastBound.includes(matrix[east[0]][east[1]])) {
-        validCoords.push(east);
+    if(allowedDirections.includes(EAST)) {
+        const eastCoord = [coord[0], coord[1] + 1];
+        if(validEastBound.includes(matrix[eastCoord[0]][eastCoord[1]])) {
+            validCoords.push(eastCoord);
+        }
     }
-    if (validDirections.includes(SOUTH) && validSouthBound.includes(matrix[south[0]][south[1]])) {
-        validCoords.push(south);
+    if(allowedDirections.includes(SOUTH)) {
+        const southCoord = [coord[0] + 1, coord[1]];
+        if(validSouthBound.includes(matrix[southCoord[0]][southCoord[1]])) {
+            validCoords.push(southCoord);
+        }
     }
-    if (validDirections.includes(WEST) && validWestBound.includes(matrix[west[0]][west[1]])) {
-        validCoords.push(west);
+    if(allowedDirections.includes(WEST)) {
+        const westCoord = [coord[0], coord[1] - 1];
+        if(validWestBound.includes(matrix[westCoord[0]][westCoord[1]])) {
+            validCoords.push(westCoord);
+        }
     }
     return validCoords;
 }
-
-function getNorth(coord: number[]): number[] {
-    return [coord[0] - 1, coord[1]];
-}
-
-function getSouth(coord: number[]): number[] {
-    return [coord[0] + 1, coord[1]];
-}
-
-function getEast(coord: number[]): number[] {
-    return [coord[0], coord[1] + 1];
-}
-
-function getWest(coord: number[]): number[] {
-    return [coord[0], coord[1] - 1];
-}
-
 /**
  * Main execution function
  */
