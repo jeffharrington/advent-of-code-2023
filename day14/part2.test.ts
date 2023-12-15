3fcbc70bf858b8bd0a50cb9d60398fb28da3c71e
Jeff Harrington <jeff@cedar.com>
2023-12-14 19:47:34 -0500

    Cleanup

--- day14/part2.test.ts
+++ day14/part2.test.ts
@@ -1,7 +1,6 @@
-import { main, matrixEqual, rotateClockwise, rotateCounterClockwise } from "./part2.js";
+import { main } from "./part2.js";
 
 describe("day14/part2", () => {
-
     it("should match test answer", async () => {
         const answer = main("input.test.txt");
         expect(answer).toBe(64);
@@ -10,38 +9,5 @@ describe("day14/part2", () => {
     it("should match final answer", async () => {
         const answer = main("input.txt");
         expect(answer).toBe(94876);
-    });
-
-    it("matrixes should be equal", async () => {
-        const matrixA = [["a"], ["b"], ["c"]];
-        const matrixB = [["a"], ["b"], ["c"]];
-        const answer = matrixEqual(matrixA, matrixB);
-        expect(answer).toBe(true);
-
-        const matrixC = "O....#....".split("").map((char) => [char]);
-        const matrixD = "O....#....".split("").map((char) => [char]);
-        const answer2 = matrixEqual(matrixC, matrixD);
-        expect(answer2).toBe(true);
-    });
-
-    it("should rotate properly", async () => {
-        const matrixA = [["a", "b", "c"], ["1", "2", "3"]];
-        const matrixB = [["a", "b", "c"], ["1", "2", "3"]];
-        const answer = matrixEqual(matrixA, matrixB);
-        expect(answer).toBe(true);
-
-        const rotatedA = rotateClockwise(matrixA);
-        console.log(rotatedA);
-        expect(matrixEqual(rotatedA, matrixB)).toBe(false);
-        expect(rotatedA).toEqual([["1", "a"], ["2", "b"], ["3", "c"]]);
-
-        const backA = rotateCounterClockwise(rotatedA);
-        expect(matrixEqual(backA, matrixA)).toBe(true);
-
-        const rotatedB = rotateClockwise(matrixB);
-        expect(matrixEqual(rotatedB, matrixA)).toBe(false);
-
-        const answer2 = matrixEqual(rotatedA, rotatedB);
-        expect(answer2).toBe(true);
     });
 });
--- day14/part2.ts
+++ day14/part2.ts
@@ -11,23 +11,21 @@ const process = (lines: string[]) => {
     const setCycles: Record<string, number> = {};
     const sumCycles: Record<string, number> = {};
     let cycleStart = null;
-    let startingKey = null;
     let cycleEnd = null;
-    for (let i = 0; i < 1000000000; i++) {
-        matrix = tilt(rotateClockwise(matrix));  // Tilt North
-        matrix = tilt(rotateClockwise(matrix));  // Tilt West
-        matrix = tilt(rotateClockwise(matrix));  // Tilt South
-        matrix = tilt(rotateClockwise(matrix));  // Tilt East
+    let startingKey = null;
 
-        const matrixToSum = rotateCounterClockwise(
-            rotateCounterClockwise(rotateCounterClockwise(matrix)),
+    for (let i = 0; i < 1000000000; i++) {
+        matrix = tilt(rotateClockwise(matrix)); // Tilt North
+        matrix = tilt(rotateClockwise(matrix)); // Tilt West
+        matrix = tilt(rotateClockwise(matrix)); // Tilt South
+        matrix = tilt(rotateClockwise(matrix)); // Tilt East
+
+        sumCycles[i] = sumMatrix(
+            rotateCounterClockwise(rotateCounterClockwise(rotateCounterClockwise(matrix))),
         );
-        const sum = sumMatrix(matrixToSum);
-        sumCycles[i] = sum;
 
         const key = matrixKey(matrix);
         if (setCycles[key] !== undefined) {
-            console.log("Cycle detected after loop", i);
             if (cycleStart == null) {
                 cycleStart = i;
                 startingKey = key;
@@ -41,36 +39,13 @@ const process = (lines: string[]) => {
     }
 
     if (!cycleStart || !cycleEnd) throw new Error("No cycle found!");
+
     const cycleLength = cycleEnd - cycleStart;
     const trueStart = cycleStart - cycleLength;
-    console.log("True Start:", trueStart);
-    console.log("Cycle length", cycleLength);
     const target = (1000000000 - trueStart) % cycleLength;
-    console.log("Target #", target);
     const targetIndex = trueStart + target - 1;
-    console.log("Target Index:", targetIndex);
-    console.log("Found:", sumCycles[targetIndex]);
-    return sumCycles[targetIndex];
-    // console.log("lastCycleDirection", lastCycleDirection);
-    // let finalMatrix = null;
-    // if (lastCycleDirection === "North") {
-    //     finalMatrix = matrix;
-    // } else if (lastCycleDirection === "West") {
-    //     finalMatrix = rotateCounterClockwise(matrix);
-    // } else if (lastCycleDirection === "South") {
-    //     finalMatrix = rotateCounterClockwise(rotateCounterClockwise(matrix));
-    // } else if (lastCycleDirection === "East") {
-    //     finalMatrix = rotateCounterClockwise(
-    //         rotateCounterClockwise(rotateCounterClockwise(matrix)),
-    //     );
-    // }
-    // if (!finalMatrix) throw new Error("No final matrix found!");
-    // // console.log("~~~~~ FINAL ~~~~~~~~");
-    // // console.log(finalMatrix.map((line) => line.join("")).join("\n"));
-    // // console.log("\n\n");
 
-    // const sum = sumMatrix(finalMatrix);
-    // return sum;
+    return sumCycles[targetIndex];
 };
 
 function matrixKey(matrix: string[][]): string {
@@ -89,29 +64,11 @@ function sumMatrix(matrix: string[][]): number {
     return sum;
 }
 
-export function matrixEqual(matrixA: string[][], matrixB: string[][]): boolean {
-    if (matrixA.length !== matrixB.length) return false;
-    if (matrixA[0].length !== matrixB[0].length) return false;
-    for (let i = 0; i < matrixA.length; i++) {
-        for (let j = 0; j < matrixA[0].length; j++) {
-            if (matrixA[i][j] !== matrixB[i][j]) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
 function tilt(matrix: string[][]): string[][] {
     const tiltedMatrix = Array.from(matrix);
     for (let i = 0; i < tiltedMatrix.length; i++) {
         let openStack: number[] = [];
         for (let j = tiltedMatrix.length - 1; j >= 0; j--) {
-            // console.log("-------------------------------------------");
-            // console.log(
-            //     `i=${i}, j=${j}, rotatedMatrix[i][j]=${tiltedMatrix[i][j]}, openStack=${openStack}`,
-            // );
-            // console.log(tiltedMatrix.map((line) => line.join("")).join("\n"));
-            // console.log("\n");
             if (tiltedMatrix[i][j] === ".") {
                 openStack.push(j);
             } else if (tiltedMatrix[i][j] === "O") {
@@ -134,11 +91,9 @@ function tilt(matrix: string[][]): string[][] {
 export function rotateClockwise(matrix: string[][]): string[][] {
     const rows = matrix.length;
     const cols = matrix[0].length;
-    // Create a new matrix with the same dimensions
     const rotatedMatrix: string[][] = new Array(cols).fill(0).map(() => new Array(rows).fill(0));
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            // Rotate each element to its new position
             rotatedMatrix[j][rows - 1 - i] = matrix[i][j];
         }
     }
@@ -148,11 +103,9 @@ export function rotateClockwise(matrix: string[][]): string[][] {
 export function rotateCounterClockwise(matrix: string[][]): string[][] {
     const rows = matrix.length;
     const cols = matrix[0].length;
-    // Create a new matrix with the same dimensions
     const rotatedMatrix: string[][] = new Array(cols).fill(0).map(() => new Array(rows).fill(0));
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            // Rotate each element to its new position
             rotatedMatrix[cols - 1 - j][i] = matrix[i][j];
         }
     }
